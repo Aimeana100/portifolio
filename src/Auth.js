a436f3014f68394c7f1ca0f7172fc336b1653346
Aimeana100 <aimeanathole@gmail.com>
2023-02-15 23:06:04 +0200

    ft(Authorize-dashboard) : protect dashboard pages
    Thsi is the implementation of the pages protection from unauthenticated users, a user whose token has expired or
    did not logged in can't access the protected pages `i.e :` add dashboard pages.
    
    - Add `Auth.js` file that call new backend API `api/auth/login/verfy` that verifies a user has a valid token.
    - if a user is logged and has not a valid token it will be redirected to the login page with the message that s/he need to login first
    - if a user is logged in has a valid token s/he will continue with the requested ressource.

--- src/Auth.js
+++ src/Auth.js
@@ -1,108 +1,15 @@
 
-// ***************************************************
-// User API
-//****************************************************/
+const Auth = {
+  saveUser : (user) => {
+    window.localStorage.setItem("user", JSON.stringify(user) );
+  }
+  ,
+  loadUser : () => {
+   return JSON.parse(window.localStorage.getItem("user") );
+  }
+}
 
-
-
-var Auth =  (function() {
-    // ===================================
-    //|| Private methods and properties ||
-    //====================================
-  
-    auth = [];
-  
-    // Constructor
-    function Item(
-      name,
-      password,
-      expires
-    ) {
-      this.name = name;
-      this.password = password;
-      this.expires = expires;
-    }
-  
-    // save Login
-    function saveAuth() {
-      window.localStorage.setItem("auth", JSON.stringify(auth));
-    }
-  
-    // load Auth from storage
-    function loadAuth() {
-        users = JSON.parse(window.localStorage.getItem("auth"));
-    }
-  
-    if (window.localStorage.getItem("auth") != null) {
-      loadAuth();
-    }
-  
-    //================================
-    // public methods and properties
-    //================================
-  
-    var obj = {};
-  
-    // Add category
-  
-    obj.Login = (
-        name,
-        password,
-        expires
-    ) => {
-        if(typeof auth[0] != "undefined"){
-            return false;
-        }
-      var item = new Item(
-        name,
-        password,
-        expires
-      );
-
-      auth[0] = item;
-      saveAuth();
-      return true;
-    };
-  
-  //   getAuth
-  obj.getUser = ((name, password) => {
-      for(let item in auth){
-          if(auth[item].name == name && auth[item].password == password ){
-              return true;
-          }
-      }
-      return false;
-  })
-  
-    //get a single user
-    obj.Logout = () => {
-        auth = [];
-        saveAuth();
-      return true;
-    };
-  
-    obj.authenticate = (loginId,loginValue ,expires)=>{
-        const d = new Date();
-        d.setTime(d.getTime() + (expires*60*60*1000));
-        let exp = "expires="+ d.toUTCString();
-        document.cookie = loginId + "=" + loginValue + ";" + exp + ";path=/";
-      
-    }
-  
-    //   remove a single user
-    obj.delete = (id) => {
-      users = users.filter((ele,index) => {
-        return ele.id != id;
-      })
-    }
-  
-   //   clear all users
-   obj.clear = ()=>{
-       users = [];
-       saveUser();
-   }
-  
-  return obj;
-
-  })();
-  
+ const isValidToken = (token) => {
+  cTs=Math.floor(Date.now() / 1000);
+  return (token>=cTs);
+}
